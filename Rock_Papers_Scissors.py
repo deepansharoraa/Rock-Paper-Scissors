a81384eb39c931060ff68043a11c29c6e176aa27
ishikaahuja145 <151758224+ishikaahuja145@users.noreply.github.com>
2025-08-15 23:44:36 +0530

    added welcome message

--- Rock_Papers_Scissors.py
+++ Rock_Papers_Scissors.py
@@ -4,7 +4,7 @@ user_wins = 0
 computer_wins = 0
 
 options = ["rock", "paper", "scissors"]
-
+print("Hello, Welcome to the game ")
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
